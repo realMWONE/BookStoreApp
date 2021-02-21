3b4ef1066bf6436a19a7b13029866711e711d9d6
unknown <bhalerah@myumanitoba.ca>
2021-02-20 18:55:28 -0600

    Book.java completed

--- app/src/main/java/com/comp3350_group10/bookstore/data/Book.java
+++ app/src/main/java/com/comp3350_group10/bookstore/data/Book.java
@@ -62,3 +62,4 @@ public class Book {
         this.stockAmount = stockAmount;
     }
 }
+
--- app/src/main/java/com/comp3350_group10/bookstore/data/BookDatabase.java
+++ app/src/main/java/com/comp3350_group10/bookstore/data/BookDatabase.java
@@ -1,27 +1,36 @@
+/**
+ * Fake Book Database
+ */
 package com.comp3350_group10.bookstore.data;
-
-import java.util.LinkedList;
+import java.util.List;
 
 public class BookDatabase {
-    LinkedList<Object> bookList = new LinkedList<Object>();
 
-    public BookDatabase(String isbn, String bookName, String bookAuthor, int price){
-        isbn = "";
-        bookName = "";
-        bookAuthor = "";
-        price = 0;
+    //List for our book database which would store the list of Book objects
+    List<Book> bookList;
+
+    //Constructor
+    public BookDatabase() {
+        //Calling createDatabase method here so everytime the BookDatabase object is created it would have all the data loaded into it
+        createDatabase();
     }
 
-    //createDatabase method: Creates a linkedlist of objects containing the books and their respective information
+    /**
+     * createDatabase: Populates the database by adding new book objects with information about the books into the list
+     */
     public void createDatabase(){
-        bookList.add(new BookDatabase("6783903121501", "Harry Potter and the Philosopher's Stone", "J.K. Rowling", 26));
-        bookList.add(new BookDatabase("6783903121502", "Harry Potter and the Chamber of secrets", "J.K. Rowling", 26));
-        bookList.add(new BookDatabase("6783903121503", "The Da Vinco Code", "Dan Brown", 30));
-        bookList.add(new BookDatabase("6783903121504", "Angels and Demons", "Dan Brown", 30));
+        bookList.add(new Book("6783903121501", "Harry Potter and the Philosopher's Stone", "J.K. Rowling", 26, 10));
+        bookList.add(new Book("6783903121502", "Harry Potter and the Chamber of secrets", "J.K. Rowling", 26, 10));
+        bookList.add(new Book("6783903121503", "The Da Vinci Code", "Dan Brown", 30, 20));
+        bookList.add(new Book("6783903121504", "Angels and Demons", "Dan Brown", 30, 5));
     }
 
-    //findBook method: Finds the books by their respective ISBN
-    public Object findBook(String isbn) {:
+    /**
+     * findBooks: Finds and returns the book objects based on their book ISBN
+     * @param isbn
+     * @return
+     */
+    public static Book findBook(String isbn) {
 
     }
 }

4c8bd79199551ca8ee9db83b668657cf89fa78aa
Kevin Davis <zorksox@gmail.com>
2021-02-20 23:51:52 -0600

    Added empty Book class. Created UIButtonFunction.java.

---
+++ app/src/main/java/com/comp3350_group10/bookstore/data/Book.java
@@ -0,0 +1,6 @@
+package com.comp3350_group10.bookstore.data;
+
+public class Book
+{
+
+}
---
+++ app/src/main/java/com/comp3350_group10/bookstore/logic/UI_Handler/UIButtonFunction.java
@@ -0,0 +1,47 @@
+package com.comp3350_group10.bookstore.logic.UI_Handler;
+
+public class UIButtonFunction implements UIHandler
+{
+
+    @Override
+    public void SearchButtonPressed(String keyword)
+    {
+
+    }
+
+    @Override
+    public void LoginButtonPressed()
+    {
+
+    }
+
+    @Override
+    public void LogoutButtonPressed()
+    {
+
+    }
+
+    @Override
+    public void UserSettingsButtonPressed()
+    {
+
+    }
+
+    @Override
+    public void IncrementStock()
+    {
+
+    }
+
+    @Override
+    public void DecrementStock()
+    {
+
+    }
+
+    @Override
+    public void SetStock()
+    {
+
+    }
+}

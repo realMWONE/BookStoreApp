7228fe4d737fab328d5fdba3bcf40caa047714d1
Kevin Davis <zorksox@gmail.com>
2021-02-21 20:52:24 -0600

    Removed nav_fragment from home screen. I will replace it with a permanent scrollView.

--- app/src/main/java/com/comp3350_group10/bookstore/MainActivity.java
+++ app/src/main/java/com/comp3350_group10/bookstore/MainActivity.java
@@ -39,9 +39,6 @@ public class MainActivity extends AppCompatActivity
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setDrawerLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
@@ -50,14 +47,6 @@ public class MainActivity extends AppCompatActivity
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
-    }
-
-    @Override
-    public boolean onSupportNavigateUp()
-    {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 
     public void onLoginButtonClicked(MenuItem item)

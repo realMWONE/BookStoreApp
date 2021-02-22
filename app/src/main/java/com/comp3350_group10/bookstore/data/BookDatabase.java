34f56ed82fe198c8fc4201879c1a5dea53e642ce
Harshal <bhalerah@myumanitoba.ca>
2021-02-22 17:21:44 -0600

    findBook complete

--- app/src/main/java/com/comp3350_group10/bookstore/data/BookDatabase.java
+++ app/src/main/java/com/comp3350_group10/bookstore/data/BookDatabase.java
@@ -2,9 +2,7 @@
  * Fake Book Database
  */
 package com.comp3350_group10.bookstore.data;
-import android.os.Build;
-
-import androidx.annotation.RequiresApi;
+import com.comp3350_group10.bookstore.data.Book;
 
 import java.util.List;
 
@@ -25,6 +23,13 @@ public class BookDatabase {
         bookList.add(new Book("6783903121502", "Harry Potter and the Chamber of secrets", "J.K. Rowling", 2650, 10));
         bookList.add(new Book("6783903121503", "The Da Vinci Code", "Dan Brown", 3000, 20));
         bookList.add(new Book("6783903121504", "Angels and Demons", "Dan Brown", 3000, 5));
+        bookList.add(new Book("6783903121505", "Diary of Wimpy Kid:The Getaway", "Jeff Kinney", 2500, 10));
+        bookList.add(new Book("6783903121506", "Diary of Wimpy Kid: Double Down", "Jeff Kinney", 2500, 10));
+        bookList.add(new Book("6783903121507", "Harry Potter and the Prisoner of Azkaban", "J.K. Rowling", 2630, 10));
+        bookList.add(new Book("6783903121508", "Harry Potter and the Goblet of Fire", "J.K. Rowling", 2630, 10));
+        bookList.add(new Book("6783903121509", "Harry Potter and the Order of Phoenix", "J.K. Rowling", 2630, 10));
+        bookList.add(new Book("6783903121510", "Harry Potter and the Half-Blood Prince", "J.K. Rowling", 2630, 10));
+        bookList.add(new Book("6783903121511", "Harry Potter and the Deathly Hallows", "J.K. Rowling", 2630, 10));
     }
 
 
@@ -33,24 +38,47 @@ public class BookDatabase {
      * @param isbn
      * @return
      */
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public static Book findBook(String isbn) {
-//        for(Book i:bookList ) {
-//
-//        }
-        return bookList.stream().filter(book -> isbn.equals(book.getBookIsbn())).findFirst().orElse(null);
+    public static String findBook(String isbn) {
+        String findByISBN = findByISBN(isbn);
+        String findbyAuthor = findByAuthor(isbn);
+        String findByTitle = findByTitle(isbn);
+        String bookInfo = findByISBN + findbyAuthor + findByTitle;
+        return bookInfo;
     }
 
+    private static String findByISBN(String isbn){
+        //Should find books by ISBN
+        String bookIsbn = null;
+        for(Book book: bookList){
+            if(book.getBookIsbn().equals(isbn)){
+                bookIsbn = book.getBookIsbn();
+            }
+        }
+        return bookIsbn;
+    }
+
+    private static String findByAuthor(String isbn){
+        //Should find books by book author
+        String bookAuthor = null;
+        for(Book book: bookList){
+            if(book.getBookIsbn().equals(isbn)){
+                bookAuthor = book.getBookAuthor();
+            }
+        }
+        return bookAuthor;
+    }
+    private static String findByTitle(String isbn){
+        //Should find books by book title
+        String bookTitle = null;
+        for(Book book: bookList){
+            if(book.getBookIsbn().equals(isbn)){
+                bookTitle = book.getBookName();
+            }
+        }
+        return bookTitle;
+    }
 }
 
-//   public static List<Book> findBooks(String searchTerm) {
-//       List1 = findByISBN(searchTerm);
-//       List2 = findByAuthor(searchTerm);
-//       List3 = findByTitle(searchTerm);
-//       MasterList = List1 + List2 + List3;
-//       MasterList.removeDuplicates();
-//       return MasterList;
-//   }
 
 
 

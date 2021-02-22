df03691c1345ec23ce10b786466aa110aa3049d6
Tamunotonye Dawari <dawarit@myumanitoba.ca>
2021-02-22 09:49:48 -0600

    Initial user commit

---
+++ app/src/main/java/com/comp3350_group10/bookstore/data/User.java
@@ -0,0 +1,49 @@
+package com.comp3350_group10.bookstore.data;
+
+import com.comp3350_group10.bookstore.UserType;
+
+public class User {
+
+    private String realName;
+    private String userID; //email
+    private String password;
+    private UserType position;
+
+    public User(String name, String userId, String password, UserType type){
+        this.realName = name;
+        this.userID = userId;
+        this.password = password;
+        this.position = type;
+    }
+
+    public String getRealName(){
+
+        return realName;
+    }
+
+    public UserType getUserType(){
+
+        return position;
+    }
+
+    public String getUserID(){
+
+        return userID;
+    }
+
+    public String getPassword(){
+
+        return password;
+    }
+
+    public void setUserID(String userId){
+
+        userID = userId;
+    }
+
+    public void setPassword(String newPassword){
+
+        password = newPassword;
+    }
+
+}

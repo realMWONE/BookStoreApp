9349cd807e26b6746d4ec287f1f87e4da6de4315
Harshal <bhalerah@myumanitoba.ca>
2021-02-23 15:55:59 -0600

    More Books added

--- app/src/main/java/com/comp3350_group10/bookstore/data/BookDatabase.java
+++ app/src/main/java/com/comp3350_group10/bookstore/data/BookDatabase.java
@@ -14,25 +14,31 @@ public class BookDatabase implements IBookDatabase{
         createDatabase();
     }
 
-    public static void setBookList(List<Book> bookList) {
-        BookDatabase.bookList = bookList;
-    }
-
     /**
      * createDatabase: Populates the database by adding new book objects with information about the books into the list
      */
     private void createDatabase(){
-        bookList.add(new Book("6783903121501", "Harry Potter and the Philosopher's Stone", "J.K. Rowling", 2630, 10));
-        bookList.add(new Book("6783903121502", "Harry Potter and the Chamber of secrets", "J.K. Rowling", 2650, 10));
-        bookList.add(new Book("6783903121503", "The Da Vinci Code", "Dan Brown", 3000, 20));
-        bookList.add(new Book("6783903121504", "Angels and Demons", "Dan Brown", 3000, 5));
-        bookList.add(new Book("6783903121505", "Diary of Wimpy Kid:The Getaway", "Jeff Kinney", 2500, 10));
-        bookList.add(new Book("6783903121506", "Diary of Wimpy Kid: Double Down", "Jeff Kinney", 2500, 10));
-        bookList.add(new Book("6783903121507", "Harry Potter and the Prisoner of Azkaban", "J.K. Rowling", 2630, 10));
-        bookList.add(new Book("6783903121508", "Harry Potter and the Goblet of Fire", "J.K. Rowling", 2630, 10));
-        bookList.add(new Book("6783903121509", "Harry Potter and the Order of Phoenix", "J.K. Rowling", 2630, 10));
-        bookList.add(new Book("6783903121510", "Harry Potter and the Half-Blood Prince", "J.K. Rowling", 2630, 10));
-        bookList.add(new Book("6783903121511", "Harry Potter and the Deathly Hallows", "J.K. Rowling", 2630, 10));
+        bookList.add(new Book("3214686513501", "Harry Potter and the Philosopher's Stone", "J.K. Rowling", 2630, 2));
+        bookList.add(new Book("1100303121502", "Harry Potter and the Chamber of secrets", "J.K. Rowling", 2650, 19));
+        bookList.add(new Book("9260783121503", "The Da Vinci Code", "Dan Brown", 3000, 20));
+        bookList.add(new Book("4458860121504", "Angels and Demons", "Dan Brown", 3000, 5));
+        bookList.add(new Book("1288903121505", "Diary of Wimpy Kid:The Getaway", "Jeff Kinney", 2500, 15));
+        bookList.add(new Book("6481103121506", "Diary of Wimpy Kid: Double Down", "Jeff Kinney", 2500, 13));
+        bookList.add(new Book("6003255121507", "Harry Potter and the Prisoner of Azkaban", "J.K. Rowling", 2630, 11));
+        bookList.add(new Book("6783908521508", "Harry Potter and the Goblet of Fire", "J.K. Rowling", 2630, 10));
+        bookList.add(new Book("6588503121509", "Harry Potter and the Order of Phoenix", "J.K. Rowling", 2630, 8));
+        bookList.add(new Book("6654684858510", "Harry Potter and the Half-Blood Prince", "J.K. Rowling", 2630, 6));
+        bookList.add(new Book("6874684025221", "Harry Potter and the Deathly Hallows", "J.K. Rowling", 2630, 6));
+        bookList.add(new Book("5354684656848", "Harry Potter and the Cursed Child", "J.K.Rowling",2730, 7));
+        bookList.add(new Book("2510323255565", "Twilight", "Stephenie Meyer", 2030, 12));
+        bookList.add(new Book("2551819816185", "Eclipse", "Stephenie Meyer", 2230, 14));
+        bookList.add(new Book("2516511685000", "New Moon", "Stephenie Meyer", 2130, 8));
+        bookList.add(new Book("2051512546452", "Breaking Dawn", "Stephenie Meyer", 2230, 7));
+        bookList.add(new Book("2510018982862", "Midnight Sun", "Stephenie Meyer", 2230, 3));
+        bookList.add(new Book("2500186257772", "The Lord of The Rings, The Fellowship of the Ring", "J. R. R. Tolkien", 2030, 5));
+        bookList.add(new Book("2500114562233", "The Lord of The Rings, The Two Towers", "J. R. R. Tolkien", 2030, 12));
+        bookList.add(new Book("2500885265433", "The Book of Lost Tales", "J. R. R. Tolkien", 2030, 13));
+        bookList.add(new Book("2369852102742", "The Children of Húrin", "J. R. R. Tolkien", 2030, 2));
     }
 
 

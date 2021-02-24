f5725c7122f7eac59e627c260f03047781df3af6
Matthew Wong <wongt348@myumanitoba.ca>
2021-02-23 20:58:12 -0600

    - Changed error messages in DataHandler from println to assert - Modified findBooks() in DataHandler, now make the keyword lowercase and separate keyword by:   - space   - dot   - dash   - comma   And passes an array of the separated words as the keyword parameter instead.

--- app/src/main/java/com/comp3350_group10/bookstore/logic/Data_Handler/DataHandler.java
+++ app/src/main/java/com/comp3350_group10/bookstore/logic/Data_Handler/DataHandler.java
@@ -1,70 +1,73 @@
 package com.comp3350_group10.bookstore.logic.Data_Handler;
-
+import java.util.*;
+import com.comp3350_group10.bookstore.UserType;
 import com.comp3350_group10.bookstore.data.Book;
+import com.comp3350_group10.bookstore.data.IBook;
+import com.comp3350_group10.bookstore.data.BookDatabase;
+import com.comp3350_group10.bookstore.data.IBookDatabase;
+import com.comp3350_group10.bookstore.data.User;
 
 import java.util.List;
 
 public class DataHandler implements Data{
 
     private User currentUser;
+    private IBookDatabase bookDatabase = new BookDatabase();
 
-    public List<Book> findBooks(String keyword){
-        return BookDatabase.findBooks(keyword);
+    //Takes the keyword and search databsse with it
+    //Will separate the keyword by spaces and dots, as well as making the keywords lower case
+    /*
+    * param keyword
+    * return list of found books
+    * */
+    public List<IBook> findBooks(String keyword){
+        return bookDatabase.findBook(keyword.toLowerCase().split("[-. ,]+"));
     }
+
 
     public void setPrice(Book target, float price){
         //make sure target is initialized
         if(target!=null){
             //price cannot be negative
-            if(price>=0){
-                target.setPrice(price);
-            }
-            else{
-                System.out.println("The price cannot be set to negative number");
-            }
+            assert price >= 0 : "The price cannot be set to negative number";
+            target.setPrice(price);
         }
     }
 
-    public void incrementPrice(Book target) {
+    public void incrementPrice(IBook target) {
         setPrice(target, target.getPrice()+1);
     }
 
-    public void decrementPrice(Book target){
+    public void decrementPrice(IBook target){
         setPrice(target, target.getPrice()-1);
     }
 
-
-    public void setStock(Book target, int quantity){
+    public void setStock(IBook target, int quantity) {
         //make sure target is initialized
-        if(target!=null){
-            //stock cannot be negative
-            if(quantity > 0){
-                target.setStock(quantity);
-            }
-            else{
-                System.out.println("The stock cannot be set to negative number");
-            }
+        if (target != null) {
+            //price cannot be negative
+            assert quantity >= 0 : "The price cannot be set to negative number";
+            target.setStock(quantity);
         }
     }
 
-
-    public void incrementStock(Book target) {
+    public void incrementStock(IBook target) {
         //make sure target is initialized
         setStock(target, target.getStock()+1);
         }
 
-    public void decrementStock(Book target){
+    public void decrementStock(IBook target){
         //Make sure target is initialized and do not decrease if stock is less than 0
         setStock(target, target.getStock()-1);
     }
 
     public boolean isCurrentUserManager(){
-        return (currentUser.getUserType() == userType.manager);
+        return (currentUser.getUserType() == UserType.Manager);
     }
 
     public void logOut(){
-        if(currentUser!=null)
-            currentUser = null;
+        assert currentUser != null : "Not logged in";
+        currentUser = null;
     }
 
 }

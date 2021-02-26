0fc003dd5a86700382da0371436a61e2a8adc804
Matthew Wong <wongt348@myumanitoba.ca>
2021-02-26 12:12:19 -0600

    Update User.java

--- app/src/main/java/com/comp3350_group10/bookstore/data/User.java
+++ app/src/main/java/com/comp3350_group10/bookstore/data/User.java
@@ -2,46 +2,54 @@ package com.comp3350_group10.bookstore.data;
 
 import com.comp3350_group10.bookstore.UserType;
 
-public class User {
+public class User 
+{
 
     private String realName;
     private String userID; //email
     private String password;
     private UserType position;
 
-    public User(String name, String userId, String password, UserType type){
+    public User(String name, String userId, String password, UserType type)
+    {
         this.realName = name;
         this.userID = userId;
         this.password = password;
         this.position = type;
     }
 
-    public String getRealName(){
+    public String getRealName()
+    {
 
         return realName;
     }
 
-    public UserType getUserType(){
+    public UserType getUserType()
+    {
 
         return position;
     }
 
-    public String getUserID(){
+    public String getUserID()
+    {
 
         return userID;
     }
 
-    public String getPassword(){
+    public String getPassword()
+    {
 
         return password;
     }
 
-    public void setUserID(String userId){
+    public void setUserID(String userId)
+    {
 
         userID = userId;
     }
 
-    public void setPassword(String newPassword){
+    public void setPassword(String newPassword)
+    {
 
         password = newPassword;
     }

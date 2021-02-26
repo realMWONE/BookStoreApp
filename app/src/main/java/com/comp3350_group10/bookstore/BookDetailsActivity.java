a45b7d3e3be5590326763de5cdd160c4fecdc6fe
zorksox <zorksox@gmail.com>
2021-02-26 10:22:27 -0600

    Removed unnecessary imports from BookDetailsActivity

--- app/src/main/java/com/comp3350_group10/bookstore/BookDetailsActivity.java
+++ app/src/main/java/com/comp3350_group10/bookstore/BookDetailsActivity.java
@@ -1,29 +1,11 @@
 package com.comp3350_group10.bookstore;
 
-import android.media.Image;
 import android.os.Bundle;
-
-import com.comp3350_group10.bookstore.data.IBook;
-import com.comp3350_group10.bookstore.logic.Data_Handler.DataHandler;
 import com.comp3350_group10.bookstore.logic.UI_Handler.BookDetailsFunctions;
 import com.comp3350_group10.bookstore.logic.UI_Handler.IBookDetailsFunctions;
-import com.comp3350_group10.bookstore.ui.ScreenSize;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.os.Parcelable;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.Space;
-import android.widget.TableRow;
-import android.widget.TextView;
-
-import java.text.DecimalFormat;
 
 public class BookDetailsActivity extends AppCompatActivity {
 
@@ -41,6 +23,5 @@ public class BookDetailsActivity extends AppCompatActivity {
 
         bookDetailsFunctions = new BookDetailsFunctions();
         bookDetailsFunctions.DrawScreen(this, IMAGE_HEIGHT, bookDetailsLayout);
-
     }
 }

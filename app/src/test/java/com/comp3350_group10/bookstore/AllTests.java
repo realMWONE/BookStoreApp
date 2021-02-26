397bb6de7e8312a38ddc9540851e068233699aa8
Matthew Wong <wongt348@myumanitoba.ca>
2021-02-26 15:53:50 -0600

    Removed residual comments AllTests.java

--- app/src/test/java/com/comp3350_group10/bookstore/AllTests.java
+++ app/src/test/java/com/comp3350_group10/bookstore/AllTests.java
@@ -9,13 +9,9 @@ import com.comp3350_group10.bookstore.data.model.UserTest;
 public class AllTests {
     //For the class LoggedInUserTest
     public static void main(String[] args){
-        //for LoggedInUserTest
-        //LoggedInUserTest.testGetUserId();
-        //LoggedInUserTest.testGetDisplayName();
-        //for BookDatabaseTest
         BookDatabaseTest.testSetBookList();
         BookDatabaseTest.testFindBook();
-        //for BookTest
+
         BookTest.testGetBookName();
         BookTest.testGetBookAuthor();
         BookTest.testGetPrice();
@@ -23,16 +19,14 @@ public class AllTests {
         BookTest.testGetStockAmount();
         BookTest.testSetPrice();
         BookTest.testSetStockAmount();
-        //for LoginDataSourceTest
-        //for LoginReposityTest
-        //for UserTest
+
         UserTest.testGetRealName();
         UserTest.testGetUserType();
         UserTest.testGetUserID();
         UserTest.testGetPassword();
         UserTest.testSetUserID();
         UserTest.testSetPassword();
-        //for DataHandlerTest
+
         DataHandlerTest.testSetPrice();
         DataHandlerTest.testIncrementPrice();
         DataHandlerTest.testDecrementPrice();

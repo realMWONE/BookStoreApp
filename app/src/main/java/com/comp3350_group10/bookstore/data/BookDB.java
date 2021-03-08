63d52d3547e22bee0161b4e87a3e8ac157f534a7
Harshal <bhalerah@myumanitoba.ca>
2021-03-07 19:14:37 -0600

    BookDatabase being worked on

---
+++ app/src/main/java/com/comp3350_group10/bookstore/data/BookDB.java
@@ -0,0 +1,99 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.util.Scanner;
+
+public class BookDB{
+
+	static Connection connection;
+	BookDatabase db = new BookDatabase();
+	
+}
+
+class BookDatabase{
+	private Connection connection;
+	private final String bookTXT = "books.txt";
+	private final String bookInfoTXT = "bookInfo.txt";
+
+	public BookDatabase(){
+		try{
+			Class.forName("ord.hsqldb.jdbcDriver");
+			//creates an in-memory database
+			connection  = DriverManager.getConenction("jdbc:hsqldb:mem:mymemdb", "SA", "");
+
+			createTables();
+			readInData();
+		}
+		catch(ClassNotFoundException e){
+			e.printStackTrace(System.out);
+		}
+		catch(SQLException e){
+			e.printStackTrace(System.out);
+		}
+	}
+
+	private void createTables(){
+		try{
+		//Creating table for books which would have the following attributes.
+		String books = "create table books ( "+
+			" bookName VARCHAR(200)," +
+			"isbn VARCHAR(15)"+
+			"quantity integer," +
+			"price integer," + 
+			"yearPublish integer," +
+			"monthPublish integer," +
+			"dayPublish integer," +
+			"reserveNumber integer," + 
+			"primary key(isbn)" + 
+			")";
+
+		String bookInfo = "create table bookInfo ( " + 
+			"isbn VARCHAR(15),"+
+			"genre VARCHAR(100),"+
+			"author VARCHAR(100),"+
+			"primary key(isbn, genre, author),"+
+			"foreign key (isbn) references books"+
+			");";
+		
+
+		connection.createStatement().executeUpdate(books);
+		connection.createStatement().executeUpdate(bookInfo);
+		}
+		catch(SQLException e){
+			e.printStackTrace(System.out);
+		}
+	}
+	
+	//readInData method: Reads data from the .txt files provided and helps populate the data.
+	private void readInData(){
+
+		BufferedReader readBooksTXT = null;
+		BufferedReader readBookInfoTXT = null;
+
+		try{
+			readBooksTXT = new BufferedReader((new FileReader(booksTXT)));
+			readBookInfo = new BufferedReader((new FileReader(bookInfoTXT)));
+			readBooksTXT.readLine();
+			readBookInfo.readLine();
+
+			String bookLine = readBooksTXT.readLine();
+			String bookInfoLine = readBookInfo.readLine();
+
+			while(bookLine != null){
+				String[] bookParts = line.split(",");
+				if(){}
+			}
+
+
+		}
+	
+	}	
+}
+

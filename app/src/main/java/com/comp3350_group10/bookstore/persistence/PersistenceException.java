5bfa3223098274a9dff8c8b64a9a45478ce4d1fb
Harshal Bhalerao <bhalerah@myumanitoba.ca>
2021-03-17 18:22:48 -0500

    Update PersistenceException.java

--- app/src/main/java/com/comp3350_group10/bookstore/persistence/PersistenceException.java
+++ app/src/main/java/com/comp3350_group10/bookstore/persistence/PersistenceException.java
@@ -1,8 +1,8 @@
 package com.comp3350_group10.bookstore.persistence;
 
 public class PersistenceException extends RuntimeException {
-    public PersistenceException(final Exception cause) {
-        super(cause);
+    public PersistenceException(final Exception e) {
+        super(e);
     }
 }
 

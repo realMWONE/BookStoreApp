78f7e3bc803af4e20e766092f05bc26e54d0e025
Harshal <bhalerah@myumanitoba.ca>
2021-03-17 20:05:13 -0500

    Main.java updated

--- app/src/main/java/com/comp3350_group10/bookstore/application/Main.java
+++ app/src/main/java/com/comp3350_group10/bookstore/application/Main.java
@@ -1,4 +1,23 @@
 package com.comp3350_group10.bookstore.application;
 
 public class Main {
+    private static String dbName="WS";
+
+    public static void setDBPath(final String name) {
+        try {
+            Class.forName("org.hsqldb.jdbc.JDBCDriver").newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        dbName = name;
+    }
+
+    public static String getDBPath() {
+        return dbName;
+    }
+
 }

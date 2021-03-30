93592abf3355a38fcdb830c2c80cab8ae3c980cd
Kevin Davis <zorksox@gmail.com>
2021-03-29 20:30:07 -0500

    Removed unnecessary class

--- app/src/main/java/com/comp3350_group10/bookstore/persistence/hsqldb/ImageReferences.java
+++
@@ -1,9 +0,0 @@
-package com.comp3350_group10.bookstore.persistence.hsqldb;
-
-import com.comp3350_group10.bookstore.R;
-
-public class ImageReferences
-{
-    public static int ANGELS_AND_DEMONS = R.drawable.angels_demons;
-
-}

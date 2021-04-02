7fefaf96dd62d87211d00a47a0b70b04c65cffe5
Harshal <bhalerah@myumanitoba.ca>
2021-04-01 21:03:34 -0500

    Changed stuff

--- app/src/test/java/com/comp3350_group10/bookstore/persistence/hsqldb/UserDatabaseStub.java
+++ app/src/test/java/com/comp3350_group10/bookstore/persistence/hsqldb/UserDatabaseStub.java
@@ -9,55 +9,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDatabaseStub implements IUserDatabase {
-    private List<IUser> list;
+
+    private List<IUser> userList;
+
     public UserDatabaseStub(){
-        this.list = new ArrayList<>();
-        list.add(new User("Daniel","duy.than@gihot.com"
-                ,"111", UserType.Employee));
-        list.add(new User("Keven","Kevin@gmail.com"
-                ,"222", UserType.Manager));
-        list.add(new User("Harshal","Harshal@myumanitoba.ca"
-                ,"333", UserType.Manager));
-        list.add(new User("Matt","Matt@yahoo.com"
-                ,"444", UserType.Employee));
-        list.add(new User("Animesh","Animesh@outlook.com"
-                ,"555", UserType.Employee));
-        list.add(new User("Darwait","Darwait@gmail.com"
-                ,"666", UserType.Employee));
+        this.userList = new ArrayList<>();
+        userList.add(new User("Daniel","duy.than@gihot.com","111", UserType.Employee));
+        userList.add(new User("Keven","Kevin@gmail.com","222", UserType.Manager));
+        userList.add(new User("Harshal","Harshal@myumanitoba.ca","333", UserType.Manager));
+        userList.add(new User("Matt","Matt@yahoo.com","444", UserType.Employee));
+        userList.add(new User("Animesh","Animesh@outlook.com","555", UserType.Employee));
+        userList.add(new User("Darwait","Darwait@gmail.com","666", UserType.Employee));
     }
 
     @Override
-    public IUser findUser(String userId) throws ClassNotFoundException {
-        for(int i=0;i<list.size();i++){
-            if(list.get(i).getUserID().equals(userId))
-                return list.get(i);
-        }
-        return null;
-    }
-
-    @Override
-    public IUser insertUser(IUser user) throws ClassNotFoundException {
-        list.add(user);
-        return user;
-    }
-
-    @Override
-    public IUser updateUser(IUser user) throws ClassNotFoundException {
-        IUser result = null;
-        for(int i=0;i<list.size();i++){
-            if(list.get(i).getUserID().equals(user.getUserID())){
-                result=list.get(i);
-                result.setName(user.getRealName());
-                result.setPassword(user.getPassword());
-                result.setPosition(user.getUserType());
-                return user;
+    public IUser findUser(String userId){
+        for(int i = 0;i < userList.size(); i++){
+            if(userList.get(i).getUserID().equals(userId)) {
+                return userList.get(i);
             }
         }
         return null;
     }
 
     @Override
-    public void deleteUser(IUser user) throws ClassNotFoundException {
+    public IUser insertUser(IUser user){
+        userList.add(user);
+        return user;
+    }
+
+    @Override
+    public IUser updateUser(IUser user){
+        int index;
+
+        index = userList.indexOf(user);
+        if(index >= 0){
+            userList.set(index, user);
+        }
+
+    }
+
+    @Override
+    public void deleteUser(IUser user){
         IUser delete=null;
         for(int i=0;i<list.size();i++){
             if(list.get(i).getUserID().equals(user.getUserID())){

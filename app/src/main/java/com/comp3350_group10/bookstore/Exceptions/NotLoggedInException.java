62858f47dd3c03bc528adef8f05c353301b92cfe
Harshal <bhalerah@myumanitoba.ca>
2021-04-06 20:22:08 -0500

    Changes made

--- app/src/main/java/com/comp3350_group10/bookstore/Exceptions/NotLoggedInException.java
+++
@@ -1,7 +0,0 @@
-package com.comp3350_group10.bookstore.Exceptions;
-
-public class NotLoggedInException extends RuntimeException{
-        public NotLoggedInException(final Exception cause) {
-            super(cause);
-        }
-}
--- app/src/main/java/com/comp3350_group10/bookstore/Exceptions/PasswordLengthException.java
+++
@@ -1,7 +0,0 @@
-package com.comp3350_group10.bookstore.Exceptions;
-
-public class PasswordLengthException extends RuntimeException{
-    public PasswordLengthException(final Exception cause) {
-        super(cause);
-    }
-}
